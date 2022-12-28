7be7f92edbf5e3335c8791080712268f98c32fa5
SaiAshish <43849911+SaiAshish9@users.noreply.github.com>
2022-12-28 23:46:48 +0530

    Add files via upload

--- RateLimiter/src/main/java/com/example/RateLimiter/RateLimiterApplication.java
+++ RateLimiter/src/main/java/com/example/RateLimiter/RateLimiterApplication.java
@@ -2,9 +2,21 @@ package com.example.RateLimiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class RateLimiterApplication {
+
+//	key used for rate limiter every time a re q comes in
+//	could be user ,host
+
+	@Bean
+	public KeyResolver keyResolver(){
+		return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+	}
+//	using host class
 
 	public static void main(String[] args) {
 		SpringApplication.run(RateLimiterApplication.class, args);
